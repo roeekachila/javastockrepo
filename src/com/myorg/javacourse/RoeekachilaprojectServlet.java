6a3277d921c3b8ea70052ccbc57e09b64cead76d
Roee Kachila <Roeekachila@gmail.com>
2014-12-25 19:22:13 +0200

    assignment 2

--- src/com/myorg/javacourse/RoeekachilaprojectServlet.java
+++ src/com/myorg/javacourse/RoeekachilaprojectServlet.java
@@ -5,7 +5,32 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class RoeekachilaprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+	
+		resp.setContentType("text/html");
+		
+		//variable declaration
+		int firstParamFirstTask, secondParamFirstTask, thirdParamFirstTask, responseFirstTask;
+		int firstParamSecondTask,secondParamSecondTask,responseSecondTask;
+		String htmlFirstTask, htmlSecondTask;
+				
+		//variable assignment		
+		firstParamFirstTask = 4;
+		secondParamFirstTask = 3;
+		thirdParamFirstTask = 7;
+		responseFirstTask = (firstParamFirstTask + secondParamFirstTask) * thirdParamFirstTask;
+		
+		firstParamSecondTask = 5;
+		secondParamSecondTask = 2;
+		responseSecondTask= firstParamSecondTask * secondParamSecondTask;
+		
+		// preparing HTML tags
+		htmlFirstTask = new String("<h1> Result of (" +firstParamFirstTask +"+"+ secondParamFirstTask + ")*" + thirdParamFirstTask + "=" + responseFirstTask + "</h1>" );
+		htmlSecondTask = new String("<h1> Result of " +firstParamSecondTask +"*"+ secondParamSecondTask + "=" + responseSecondTask + "</h1>" );
+
+		// add to HTML
+		resp.getWriter().println(htmlFirstTask);
+		resp.getWriter().println(htmlSecondTask);
+
+		
 	}
 }
